a3cb8c66e7bd21f2a50fb0e62e4fc2952570d436
joehillen <joehillen@gmail.com>
2010-10-22 07:30:58 -0700

    New working crawlerscript using multiprocessing.

--- crawlerscript.py
+++ crawlerscript.py
@@ -6,6 +6,7 @@ from scrapy import log, signals, project
 from scrapy.xlib.pydispatch import dispatcher
 from scrapy.conf import settings
 from scrapy.crawler import CrawlerProcess
+from multiprocessing import Process, Queue
 
 class CrawlerScript():
 
@@ -18,26 +19,32 @@ class CrawlerScript():
 		dispatcher.connect(self._item_passed, signals.item_passed)
 
     def _item_passed(self, item):
-        self.items.append(item)
+    	self.items.append(item)
 
-    def start(self):
-    	self.crawler.start()
-    
-    def stop(self):
-    	self.crawler.stop()
-
-    def addSpider(self, spider_name):
+    def crawl(self, queue, spider_name):
         spider = self.crawler.spiders.create(spider_name)
         if spider:
             self.crawler.queue.append_spider(spider)
-            
+    	self.crawler.start()
+        self.crawler.stop()
+        queue.put(self.items)
+        
+
+class CrawlerP():
+		
+	def crawl(self, spider):
+		queue = Queue()
+		c = CrawlerScript()
+		p = Process(target=c.crawl, args=(queue, spider,))
+		p.start()
+		p.join()
+		return queue.get()
+
 # Usage
 if __name__ == "__main__":
 	log.start()
 
-	c = CrawlerScript()
-	c.addSpider('pic')
-	c.addSpider('album')
-	c.start()
-	print c.items
-	c.stop()
+	items = list()
+	crawler = CrawlerP()
+	items.append(crawler.crawl('pic'))
+	print items
