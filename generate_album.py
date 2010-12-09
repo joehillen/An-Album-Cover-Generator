013b3d8caa94a6231be6ad73f4825a49a89f6b9f
joehillen <joehillen@gmail.com>
2010-12-09 14:58:02 -0800

    Improve generator script

--- generate_album.py
+++ generate_album.py
@@ -6,12 +6,24 @@ import string, sys
 from crawlerscript import *
 
 crawler = CrawlerScript()
-pic_item = crawler.crawl('pic')[0]
+
+# Get Pic
+pic_results = crawler.crawl('pic')
+pic_item = pic_results[0] if len(pic_results) >= 1 else None
 print pic_item
-band_item = crawler.crawl('band')[0]
+
+# Get Band Name
+band_results = crawler.crawl('band')
+band_item = band_results[0] if len(band_results) >= 1 else None
 print band_item
-album_item = crawler.crawl('album')[0]
+
+# Get Album Name
+album_results = crawler.crawl('album')
+album_item = album_results[0] if len(album_results) >= 1 else None
 print album_item
 
-create = CoverCreator()
-create.cover('/tmp/albumgen/' + pic_item['pic_path'], band_item['band'], album_item['album'])
+if pic_item and band_item and album_item:
+    create = CoverCreator()
+    create.cover('/tmp/albumgen/' + pic_item['pic_path'], band_item['band'], album_item['album'])
+else:
+    print "Failed to generate album"
